d337ed788db1dcc3bb405c2dbae979c957cb5299
shelbel22 <40868414+shelbel22@users.noreply.github.com>
2018-09-20 20:19:00 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -30,7 +30,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 ## Computes the inverse
-
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
  inv <- x$getInverse()
 
   if(!is.null(inv)){
